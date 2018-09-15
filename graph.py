f4ab1f0f3cadd232163b740f599f34bf8e120c48
ho.bahram@gmail.com <ho.bahram@gmail.com>
2018-09-15 09:43:23 +0800

    readme added, initialized graph.py content

---
+++ graph.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Module Docstring
+"""
+
+__author__ = "Hossein Bahrami Nekoo"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print "hello world"
+
+
+if __name__ == "__main__":
+
+    main()
